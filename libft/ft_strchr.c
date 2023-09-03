301a71c174b69951347bf46195c30f116691192f
Jay <jay.ji.jaroen@gmail.com>
2023-09-03 21:48:03 +0700

    finish function strchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jjaroens <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jay <jay@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 15:40:46 by jjaroens          #+#    #+#             */
-/*   Updated: 2023/09/02 15:55:08 by jjaroens         ###   ########.fr       */
+/*   Updated: 2023/09/03 21:47:35 by jay              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,40 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	unsigned char *str;
-	char	word;
+	unsigned char a;
+	char	*str;
+  int i;
 
+  a = (unsigned char)c;
+  str = (char *)s;
+  i = 0;
+  while (str[i] != '\0')
+  {
+    if (str[i] == a)
+      return(&str[i]);
+    i++;
+  }
+  return (NULL);
 }
 
-int	main(void)
+/*int	main(void)
 {
 	char word[] = "Hello world";
-	char target = 'w';
+	char target = 'W';
+  char word2[] = "Hello world";
+	char target2 = 'W';
 	char *result;
+  char *result2;
 
 	result = strchr(word,target);
-	printf("%c\n", *result);
-}
+  result2 = ft_strchr(word2, target2);
+  if (result != NULL)
+    printf("the result of strchr: %c\n", *result);
+  if (result == NULL)
+    printf("not found\n");
+  if (result2 != NULL)
+    printf("the result of my function: %c\n", *result2);
+  if (result2 == NULL)
+    printf("not found\n");
+  return(0);
+}*/
