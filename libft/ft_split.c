b7adda302f1d6cf827a0c1f058a3863fdb5e3bc3
Jay <jay.ji.jaroen@gmail.com>
2023-09-15 23:34:10 +0700

    add ft_split.c

---
+++ libft/ft_split.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jay <jay@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/15 23:14:11 by jay               #+#    #+#             */
+/*   Updated: 2023/09/15 23:33:17 by jay              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+//parameters: 1) s = the string to be split; 2) c = the delimiter character
+//return value = the array oof new strings resulting from the split
+// 1. need to know the number of substring in C
+// 2. allocate the memory for the array
+// 3. calculate the memory for the substring
+// 4. allocate the memory for the substring
+// 5. use the strlcpy to copy the substring into the string
+// 6. handle the last substring
+char  **ft_split(char const *s, char c)
+{
+
+}
