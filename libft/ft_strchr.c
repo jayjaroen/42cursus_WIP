1df459ed349d86e3837041581f1230bb030d8116
Jay <jay.ji.jaroen@gmail.com>
2023-09-19 21:39:07 +0700

    edit ft_strchr.c

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -11,44 +11,18 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <string.h>
-
+// Description: The strchr() function locates the first occurrence of c 
+//(converted to a char) in the string pointed to by s.
+// The terminating null character is considered to be part of the string;
+// therefore if c is ‘\0’, the functions locate the terminating ‘\0’.
+// Return: The functions strchr() and strrchr() return a pointer to the
+// located character, or NULL if the character does not appear in the string.
 char	*ft_strchr(const char *s, int c)
 {
-	unsigned char a;
-	char	*str;
-  int i;
-
-  a = (unsigned char)c;
-  str = (char *)s;
-  i = 0;
-  while (str[i] != '\0')
-  {
-    if (str[i] == a)
-      return(&str[i]);
-    i++;
-  }
-  return (NULL);
+	while (*s && *s != (char)c)
+		s++;
+	if (*s == (char)c || (!*s && (char)c == 0))
+		return ((char *)s);
+	else
+		return (NULL);
 }
-/*int	main(void)
-{
-	char word[] = "Hello world";
-	char target = 'W';
-  char word2[] = "Hello world";
-	char target2 = 'W';
-	char *result;
-  char *result2;
-
-	result = strchr(word,target);
-  result2 = ft_strchr(word2, target2);
-  if (result != NULL)
-    printf("the result of strchr: %c\n", *result);
-  if (result == NULL)
-    printf("not found\n");
-  if (result2 != NULL)
-    printf("the result of my function: %c\n", *result2);
-  if (result2 == NULL)
-    printf("not found\n");
-  return(0);
-}*/
