968650b2f4ce467a5008a77b2be7d76536bd03f4
Jay <jay.ji.jaroen@gmail.com>
2023-09-19 22:59:38 +0700

    edit strrchr.c not final

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -11,45 +11,21 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "string.h"
-#include "stdio.h"
-
-char  *ft_strrchr(const char *s, int c)
+// Description: The strrchr() function is identical to strchr(), 
+//except it locates the last occurrence of c.
+// Return value: The functions strchr() and strrchr() return a pointer
+// to the located character, or NULL if the character does not appear in the string.
+char	*ft_strrchr(const char *s, int c)
 {
-  unsigned char w;
-  char  *str;
-  size_t i;
-
-  i = ft_strlen(s) - 1;
-  w = (unsigned char)c;
-  str = (char*)s;
-  while (i)
-  {
-    if (str[i] == w)
-      return(&str[i]);
-    i--;
-  }
-  return (NULL);
+	size_t	i;
+	
+	i = ft_strlen(s);
+	while (i != 0 && *(s + i) != c)
+		i--;
+	if (c == *(s + i))
+		return ((char *)(s + i));
+	else if ( c == *s)
+		return ((char *)s);
+	else
+		return (NULL);
 }
-
-/*int	main(void)
-{
-	char word[] = "Hello worlddd4";
-	char target = '4';
-  char word2[] = "Hello worldd4";
-	char target2 = '4';
-	char *result;
-  char *result2;
-
-	result = strrchr(word,target);
-  result2 = ft_strrchr(word2, target2);
-  if (result != NULL)
-    printf("the result of strchr: %c\n", *result);
-  if (result == NULL)
-    printf("not found\n");
-  if (result2 != NULL)
-    printf("the result of my function: %c\n", *result2);
-  if (result2 == NULL)
-    printf("not found\n");
-  return(0);
-}*/
