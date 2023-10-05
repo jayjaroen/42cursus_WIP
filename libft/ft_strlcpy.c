e8ebb81748c07665033cb77ac61f5a67aae9f336
Jay <jay.ji.jaroen@gmail.com>
2023-10-05 09:21:40 +0700

    edit ft_strtrim.c

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: jay <jay@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 11:19:43 by jjaroens          #+#    #+#             */
-/*   Updated: 2023/09/16 22:49:56 by jay              ###   ########.fr       */
+/*   Updated: 2023/10/04 23:36:38 by jay              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 
 	srcsize = ft_strlen(src);
 	if (!(src) || !(dst) || !dstsize)
-		return (srcsize);
+	  return (srcsize);
 	i = 0;
 	while (src[i] && (i < dstsize - 1))
 	{
@@ -30,15 +30,3 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	dst[i] = '\0';
 	return (srcsize);
 }
-/*int	main(void)
-{
-	char word[10];
-	char src[] = "hellohellohello";
-  char word1[10];
-	char src1[] = "hellohellohello";
-
-	printf("strlcpy funtion:%zu\n", strlcpy(word, src, 0));
-  printf("strclpy function:%s\n", word);
-	printf("strlcpy funtion:%zu\n", ft_strlcpy(word1, src1, 0));
-  printf("strclpy function:%s\n", word1);
-}*/
--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,40 +6,42 @@
 /*   By: jay <jay@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 12:22:54 by jay               #+#    #+#             */
-/*   Updated: 2023/09/26 22:55:19 by jay              ###   ########.fr       */
+/*   Updated: 2023/10/05 09:13:49 by jay              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
-Descritption: Allocates (with malloc(3)) and returns a copy of
-’s1’ with the characters specified in ’set’ removed
+/*Descritption: Allocates (with malloc(3)) and returns a copy
+of’s1’ with the characters specified in ’set’ removed
 from the beginning and the end of the string.
 Parameters: s1: The string to be trimmed
             set: The reference set of characters to trim
 return: The trimmed string.
         NULL if the allocation fails.
 1. check if the set character is found in S1 at the begining and at the end
-2. identify the location of where to trim the string by locating the start & end point
+2. identify the location of where to trim the string by locating the start 
+& end point
 3. how to deal with full blank / empty blank input
 */
-char  *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-  char  *trim;
-  size_t  start;
-  size_t  end;
+	char	*trim;
+	size_t	start;
+	size_t	end;
+	size_t	i;
 
-  if (!s1 || !set)
-    return (NULL);
-  start = 0;
-  end = ft_strlen(s1) - 1;
-  while (start < end && ft_strchr(set, s1[start]) != NULL) // if NULL then not found the match
-    start++;
-  while (end > start && ft_strchr(set, s1[end]) != NULL)
-    end--;
-  trim = (char *)malloc(sizeof(char) * (end - start + 2)); //index + null terminator
-  if (trim == NULL)
-    return (NULL);
-  ft_strlcpy(trim, s1 + start, end - start + 2); // does it work with blank input / tab
-  return (trim);
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, *(s1 + start)))
+		start++;
+	while (end > start && ft_strchr(set, *(s1 + end - 1)))
+		end--;
+	trim = (char *)malloc(sizeof(char) * (end - start + 1));
+	if (trim == NULL)
+		return (NULL);
+	i = 0;
+	ft_strlcpy(trim, s1 + start, end - start + 1);
+	return (trim);
 }
