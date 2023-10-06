a1b6daf3b05160f369b6f0ba4a1ee533309dacf8
Jay <jay.ji.jaroen@gmail.com>
2023-10-06 23:24:53 +0700

    edit ft_split.c

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -3,89 +3,100 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jjaroens <jjaroens@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jay <jay@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 23:14:11 by jay               #+#    #+#             */
-/*   Updated: 2023/09/16 13:54:05 by jjaroens         ###   ########.fr       */
+/*   Updated: 2023/10/06 23:04:36 by jay              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
-#include <stdio.h>
-//parameters: 1) s = the string to be split; 2) c = the delimiter character
-//return value = the array oof new strings resulting from the split
-// 1. need to know the number of substring in C
-// 2. allocate the memory for the array, only a pointer
-// 3. calculate the memory for the substring strlen
-// 4. allocate the memory for the substring, an index of a pointer to another pointer?i
-// 5. use the strlcpy to copy the substring into the string
-// 6. handle the last substring
-int	sub_string(char const *s, char c)
+/*
+Description: Allocates (with malloc(3)) and returns an array
+of strings obtained by splitting ’s’ using the
+character ’c’ as a delimiter.  The array must end
+with a NULL pointer.
+Parameters: 1) s = the string to be split; 2) c = the delimiter character
+Return: the array oof new strings resulting from the split
+        NULL if the allocation fails.
+*/
+static int	ft_check(char s, char c)
 {
-	int	i;
-	int	count;
+	return (s == c);
+}
 
-	i = 0;
+static size_t	ft_count_sub(char const *s, char c)
+{
+	size_t	count;
+	size_t	i;
+
 	count = 0;
-	while (s[i] != '\0')
+	i = 0;
+	while (*(s + i) != '\0')
 	{
-		if (s[i] == c)
+		while (*(s + i) != '\0' && *(s + i) == c)
+			i++;
+		if (*(s + i) != '\0')
 			count++;
-		i++;
+		while (*(s + i) != '\0' && *(s + i) != c)
+			i++;
 	}
 	return (count);
+}
+
+static size_t	word_len(char const *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (*(s + i) != '\0' && *(s + i) != c)
+		i++;
+	return (i);
+}
+
+static char	*sub(char const *s, char c)
+{
+	size_t	len;
+	size_t	i;
+	char	*word;
+
+	len = word_len(s, c);
+	word = (char *)malloc(sizeof(char) * (len + 1));
+	if (word == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		*(word + i) = *(s + i);
+		i++;
+	}
+	*(word + i) = '\0';
+	return (word);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**new;
-	int		substring;
-	char	*string;
-	char	*current;
-	int		index;
+	size_t	i;
 
-	if (!*s || !c)
+	if (s == NULL)
 		return (NULL);
-	substring = sub_string(s, c) + 1;
-	new = (char **)malloc(sizeof(char*) * substring);
+	new = (char **)malloc(sizeof(char *) * (ft_count_sub (s, c) + 1));
 	if (new == NULL)
-		return NULL;
-	index = 0;
-	string = (char *)s;
-	current = (char *)string;
-	while (*current != '\0')
-	{
-		if (*current == c)
-		{
-			new[index] = (char *)malloc(sizeof(char)* ft_strlen(current) + 1);
-			if (new[index] == NULL)
-				return (NULL);
-			ft_strlcpy(new[index], string, current - string );
-			new[index][current - string] = '\0';
-			string = current + 1;
-			index++;
-		}
-		current++;
-	}
-	new[index] = (char *)malloc(sizeof(char)* ft_strlen(current) + 1);
-	if (new[index] == NULL)
 		return (NULL);
-	ft_strlcpy(new[index], string, current - string);
-	new[index][current - string] = '\0';
+	i = 0;
+	while (*s)
+	{
+		while (*s != '\0' && ft_check (*s, c))
+			s++;
+		if (*s != '\0')
+		{
+			*(new + i) = sub (s, c);
+			i++;
+		}
+		while (*s != '\0' && ft_check (*s, c) == 0)
+			s++;
+	}
+	*(new + i) = 0;
 	return (new);
 }
-/*int	main(void)
-{
-	char	*word = "Hello ! World ! Babaaa ! baa";
-	char	c = '!';
-	char	**result;
-	int		i;
-
-	result = ft_split(word, c);
-	for ( i = 0; result[i] != NULL; i++)
-	{
-		printf("%s\n", result[i]);
-	}
-	return (0);
-}*/
